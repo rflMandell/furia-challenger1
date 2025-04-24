b209d1e81daf2847d4f03f149c2414310a3e6ab4
Rafael Mandel <rafa80744@gmail.com>
2025-04-24 17:20:46 -0300

    .

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from core.models import User #impor do modelo de user do core
 
 # Create your models here.
+
+class Chat(models.Model):
+    name = models.CharField(max_length=100) #nome do chat, vai ta como cs, lol etc...
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, related_name="messages", on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Message from {self.sender.username} in {self.chat.name}"
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    is_fan = models.BooleanField(default=True) #para users normais
+    is_admin = models.BooleanField(default=False) #para admins
+    #em breve mais campo?? talvez :)
+    
